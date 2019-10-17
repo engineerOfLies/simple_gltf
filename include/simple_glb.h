5f12d9aaaa5710dda3e38bb7670c4f90f1a08a1f
DJ Kehoe <donald.j.kehoe@gmail.com>
2019-10-17 11:41:36 -0400

    still setting up

--- include/simple_glb.h
+++ include/simple_glb.h
@@ -3,12 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct
 {
-    uint32 magic;       /**<equals 0x46546C67. It is ASCII string glTF*/
-    uint32 version;     /**<indicates the version of the Binary glTF container format.*/
-    uint32 length;      /**<is the total length of the Binary glTF, including Header and all Chunks, in bytes.*/
+    int32_t magic;       /**<equals 0x46546C67. It is ASCII string glTF*/
+    int32_t version;     /**<indicates the version of the Binary glTF container format.*/
+    int32_t length;      /**<is the total length of the Binary glTF, including Header and all Chunks, in bytes.*/
 }GLB_Header;
 
 typedef struct
@@ -30,4 +31,6 @@ GLB_File *simple_glb_load(char * filepath);
  * @param glbFFile the glb data to free
  */
 void simple_glb_free(GLB_File *glbFile);
-#define
+
+
+#endif
--- src/simple_glb.c
+++ src/simple_glb.c
@@ -1,13 +1,55 @@
+#include <string.h>
 #include "simple_logger.h"
 #include "simple_glb.h"
+
+GLB_File *simple_glb_new()
+{
+    GLB_File *glbFile = NULL;
+    glbFile = (GLB_File*)malloc(sizeof(GLB_File));
+    if (!glbFile)
+    {
+        slog("failed to allocate space for GLP FILE");
+        return NULL;
+    }
+    memset(glbFile,0,sizeof(GLB_File));
+    return glbFile;
+}
 
 GLB_File *simple_glb_load(char * filepath)
 {
     GLB_File *glbFile = NULL;
-    return NULL;
+    FILE *file;
+    file = fopen(filepath,"rb");
+    if (!file)
+    {
+        slog("failed to open file %s",filepath);
+        return NULL;
+    }
+    glbFile = simple_glb_new();
+    if (!glbFile)
+    {
+        fclose(file);
+        return NULL;
+    }
+    
+    fclose(file);
+    return glbFile;
 }
 
-void simple_glb_free(GLB_File *glbFile);
+void simple_glb_free(GLB_File *glbFile)
+{
+    if (!glbFile)
+    {
+        slog("no glbFile handle provided");
+        return;
+    }
+    if (glbFile->buffer)
+    {
+        free(glbFile->buffer);
+        glbFile->buffer = NULL;
+    }
+    free(glbFile);
+}
 
 
 /*eol@eof*/
